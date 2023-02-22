346e59fbcc8f26a1796698551d132cbc7d0aa035
TheWerbinator <34259061+TheWerbinator@users.noreply.github.com>
2023-02-21 17:33:56 -0700

    Update Checkout.jsx
    Format and syntax changes

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,27 +1,45 @@
 import React from "react";
-import Cart from './Cart/Cart';
-import Shipping from './Shipping/Shipping';
-import Payment from './Payment/Payment';
-import Confirmation from './Confirmation/Confirmation';
-import './Checkout.css';
+import Cart from "./Cart/Cart";
+import Shipping from "./Shipping/Shipping";
+import Payment from "./Payment/Payment";
+import Confirmation from "./Confirmation/Confirmation";
+import "./Checkout.css";
 import Login from "./Login/Login";
-import pic1 from './assets/code1.png'
-import pic2 from './assets/code2.png'
-import pic3 from './assets/code3.png'
+import pic1 from "./assets/code1.png";
+import pic2 from "./assets/code2.png";
+import pic3 from "./assets/code3.png";
 
-const checkoutRoutes = [
-  'login',
-  'cart',
-  'shipping',
-  'payment',
-  'confirmation',
-]
+const checkoutRoutes = ["login", "cart", "shipping", "payment", "confirmation"];
 
 const products = [
-  { image: pic1, name: 'CSS Refactoring', color: 'Color: Black', size: 'Size: 4', price: 250, quantity: 1, total: 250},
-  { image: pic2, name: 'HTML Overhaul', color: 'Color: White', size: 'Size: 5', price: 500, quantity: 1, total: 500},
-  { image: pic3, name: 'JS Rework', color: 'Color: Green', size: 'Size: 6', price: 750, quantity: 1, total: 750},
-]
+  {
+    image: pic1,
+    name: "CSS Refactoring",
+    color: "Color: Black",
+    size: "Size: 4",
+    price: 250,
+    quantity: 1,
+    total: 250,
+  },
+  {
+    image: pic2,
+    name: "HTML Overhaul",
+    color: "Color: White",
+    size: "Size: 5",
+    price: 500,
+    quantity: 1,
+    total: 500,
+  },
+  {
+    image: pic3,
+    name: "JS Rework",
+    color: "Color: Green",
+    size: "Size: 6",
+    price: 750,
+    quantity: 1,
+    total: 750,
+  },
+];
 
 class Checkout extends React.Component {
   state = {
@@ -32,168 +50,189 @@ class Checkout extends React.Component {
     shipping: 0,
     discount: 0,
     promoApplied: false,
-    promo: '',
+    promo: "",
     total: products[0].total + products[1].total + products[2].total,
     empty: false,
-    lastFour: '',
-    cardType: 'VISA',
-    customerEmails: [
-      {email: 'jon@jon.com',
-       password: 'asdf123!'}
-    ]
-  }
+    lastFour: "",
+    cardType: "VISA",
+    customerEmails: [{ email: "jon@jon.com", password: "asdf123!" }],
+  };
 
   handleRouteChange = (change) => {
-    this.setState({checkoutRouteIndex: change})
-  }
-  
+    this.setState({ checkoutRouteIndex: change });
+  };
+
   handleAuth = (open) => {
-    open === 1 ? this.setState({openAuth: true}) : this.setState({openAuth: false})
-  }
-  
+    open === 1
+      ? this.setState({ openAuth: true })
+      : this.setState({ openAuth: false });
+  };
+
   handleLoggedIn = () => {
     this.setState({
       loggedIn: true,
-    })
-    this.handleAuth(0)
-  }
+    });
+    this.handleAuth(0);
+  };
 
   handleSubtotal = () => {
     let tempSubtotal = 0;
-    products.forEach((product) => tempSubtotal += product.total)
+    products.forEach((product) => (tempSubtotal += product.total));
     let tempTotal = 0;
-    products.forEach((product) => tempTotal += product.total)
+    products.forEach((product) => (tempTotal += product.total));
     this.setState({
       subtotal: tempSubtotal,
-      total: tempTotal - this.state.discount + this.state.shipping
-    })
-  }
+      total: tempTotal - this.state.discount + this.state.shipping,
+    });
+  };
 
   handleShipping = (type) => {
-    type === 'Express' ? this.setState({shipping: 5}) 
-      : this.setState({shipping: 0})
-    type === 'Express' ? this.setState({total: this.state.subtotal - this.state.discount + 5}) 
-    : this.setState({total: this.state.subtotal - this.state.discount})
-  }
+    type === "Express"
+      ? this.setState({ shipping: 5 })
+      : this.setState({ shipping: 0 });
+    type === "Express"
+      ? this.setState({ total: this.state.subtotal - this.state.discount + 5 })
+      : this.setState({ total: this.state.subtotal - this.state.discount });
+  };
 
   handleTotals = (index) => {
-    products[index].total = (products[index].quantity * products[index].price)
-    this.setState({total: this.state.subtotal - this.state.discount + this.state.shipping})
-  }
+    products[index].total = products[index].quantity * products[index].price;
+    this.setState({
+      total: this.state.subtotal - this.state.discount + this.state.shipping,
+    });
+  };
 
   removeProduct = (index) => {
-    products.splice(index, 1)
-    this.handleSubtotal()
-  }
+    products.splice(index, 1);
+    this.handleSubtotal();
+  };
 
   handleEmpty = () => {
-    this.setState({empty: true})
-  }
+    this.setState({ empty: true });
+  };
 
   handlePromo = (code, applied) => {
-    this.setState({discount: 10}, () => {
-      this.handleSubtotal()
-    })
-    this.setState({promo: code})
-    applied === true && this.setState({promoApplied: true})
-  }
+    this.setState({ discount: 10 }, () => {
+      this.handleSubtotal();
+    });
+    this.setState({ promo: code });
+    applied === true && this.setState({ promoApplied: true });
+  };
 
   confirmationInfo = (cardData) => {
-    const last4 = cardData.card.slice(-4)
-    this.setState({lastFour: last4})
-  }
+    const last4 = cardData.card.slice(-4);
+    this.setState({ lastFour: last4 });
+  };
 
   handleCardType = (type) => {
-    this.setState({cardType: type})
-  }
+    this.setState({ cardType: type });
+  };
 
   handleAddAccount = (email, password) => {
-    let newAccount = this.state.customerEmails.map()
+    let newAccount = this.state.customerEmails.map();
     console.log(newAccount);
 
-    newAccount.push()
-  }
+    newAccount.push();
+  };
 
   render() {
     const checkoutRoute = checkoutRoutes[this.state.checkoutRouteIndex];
 
     const handleQuantity = (value, index) => {
-      products[index].quantity = value
-      this.handleTotals(index)
-      this.handleSubtotal()
-    }
+      products[index].quantity = value;
+      this.handleTotals(index);
+      this.handleSubtotal();
+    };
 
     return (
-      <div className="checkout-body">
-        <div className="cart-header">
-          {this.state.loggedIn ?
+      <div className='checkout-body'>
+        <div className='cart-header'>
+          {this.state.loggedIn ? (
             <div>
-              <button className="profile-button">View Profile</button>
+              <button className='profile-button'>View Profile</button>
             </div>
-          : <div className="login-buttons">
-              <button className="login-button" onClick={() => {this.handleAuth(1)}}>Log In</button>
+          ) : (
+            <div className='login-buttons'>
+              <button
+                className='login-button'
+                onClick={() => {
+                  this.handleAuth(1);
+                }}
+              >
+                Log In
+              </button>
             </div>
-          }
+          )}
         </div>
 
         <div>
-          {this.state.openAuth ? 
-          <Login 
-            handleLoggedIn={this.handleLoggedIn} 
-            handleAuth={this.handleAuth}
-            existingAccounts={this.state.customerEmails}/>
-          : checkoutRoute === 'cart' && 
-          <Cart 
-            products={products} 
-            route={this.handleRouteChange} 
-            subtotal={this.state.subtotal} 
-            handleAuth={this.handleAuth} 
-            loggedIn={this.state.loggedIn}
-            shipping={this.state.shipping}
-            discount={this.state.discount}
-            handlePromo={this.handlePromo}
-            promo={this.state.promo}
-            promoApplied={this.state.promoApplied}
-            quantity={handleQuantity}
-            total={this.state.total} 
-            remove={this.removeProduct}
-            empty={this.state.empty}
-            handleEmpty={this.handleEmpty}/>}
-          {checkoutRoute === 'shipping' && 
-          <Shipping 
-            products={products} 
-            route={this.handleRouteChange} 
-            subtotal={this.state.subtotal}
-            shipping={this.state.shipping}
-            discount={this.state.discount}
-            handleShipping={this.handleShipping}  
-            total={this.state.total}
-            currentPlace={this.state.checkoutRouteIndex}/>}
-          {checkoutRoute === 'payment' && 
-          <Payment 
-            products={products} 
-            route={this.handleRouteChange} 
-            subtotal={this.state.subtotal}
-            shipping={this.state.shipping}
-            discount={this.state.discount}
-            total={this.state.total}
-            confirmationInfo={this.confirmationInfo}
-            handleCardType={this.handleCardType}
-            currentPlace={this.state.checkoutRouteIndex}/>}
-          {checkoutRoute === 'confirmation' && 
-          <Confirmation 
-            products={products} 
-            subtotal={this.state.subtotal}
-            shipping={this.state.shipping}
-            discount={this.state.discount}
-            total={this.state.total}
-            lastFour={this.state.lastFour}
-            cardType={this.state.cardType}
-            currentPlace={this.state.checkoutRouteIndex}/>}
+          {this.state.openAuth ? (
+            <Login
+              handleLoggedIn={this.handleLoggedIn}
+              handleAuth={this.handleAuth}
+              existingAccounts={this.state.customerEmails}
+            />
+          ) : (
+            checkoutRoute === "cart" && (
+              <Cart
+                products={products}
+                route={this.handleRouteChange}
+                subtotal={this.state.subtotal}
+                handleAuth={this.handleAuth}
+                loggedIn={this.state.loggedIn}
+                shipping={this.state.shipping}
+                discount={this.state.discount}
+                handlePromo={this.handlePromo}
+                promo={this.state.promo}
+                promoApplied={this.state.promoApplied}
+                quantity={handleQuantity}
+                total={this.state.total}
+                remove={this.removeProduct}
+                empty={this.state.empty}
+                handleEmpty={this.handleEmpty}
+              />
+            )
+          )}
+          {checkoutRoute === "shipping" && (
+            <Shipping
+              products={products}
+              route={this.handleRouteChange}
+              subtotal={this.state.subtotal}
+              shipping={this.state.shipping}
+              discount={this.state.discount}
+              handleShipping={this.handleShipping}
+              total={this.state.total}
+              currentPlace={this.state.checkoutRouteIndex}
+            />
+          )}
+          {checkoutRoute === "payment" && (
+            <Payment
+              products={products}
+              route={this.handleRouteChange}
+              subtotal={this.state.subtotal}
+              shipping={this.state.shipping}
+              discount={this.state.discount}
+              total={this.state.total}
+              confirmationInfo={this.confirmationInfo}
+              handleCardType={this.handleCardType}
+              currentPlace={this.state.checkoutRouteIndex}
+            />
+          )}
+          {checkoutRoute === "confirmation" && (
+            <Confirmation
+              products={products}
+              subtotal={this.state.subtotal}
+              shipping={this.state.shipping}
+              discount={this.state.discount}
+              total={this.state.total}
+              lastFour={this.state.lastFour}
+              cardType={this.state.cardType}
+              currentPlace={this.state.checkoutRouteIndex}
+            />
+          )}
         </div>
-
       </div>
-    )
+    );
   }
 }
 
